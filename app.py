295218a3aa246c257364089b758744e968e674dc
Gloww <91305530+Glowwonyt@users.noreply.github.com>
2022-03-08 21:10:30 +0530

    Changed app.py coz  ¯\_ (ツ)_/¯

--- app.py
+++ app.py
@@ -1,15 +1,64 @@
-from quart_discord import DiscordOAuth2Session
-import  quart
-from quart import  Quart , render_template , websocket
+import discord
+from quart import  Quart , render_template , url_for , redirect
+from quart_discord import DiscordOAuth2Session, requires_authorization, Unauthorized
 import asyncio
-
+import os
+import random
+from discord.ext import ipc
 
 app = Quart(__name__)
 
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"
+
+app.secret_key = b"shhh"
+app.config['DISCORD_CLIENT_ID'] = 822039023584673813
+app.config['DISCORD_CLIENT_SECRET'] = 'Add ur secret key'
+app.config['DISCORD_REDIRECT_URI'] = "http://127.0.0.1:5000/callback"
+app.config['DISCORD_BOT_TOKEN'] = 'Put ur token here'
+
+discord = DiscordOAuth2Session(app)
+myipc = ipc.Client(secret_key="dashmod")
+
+
 @app.route("/")
 async def main_page():
-    return render_template("main.html")
+    return await render_template("index.html")
 
+@app.route("/dashboard")
+@requires_authorization
+async def dashboard():
+    welcomes = ["We've Been waiting",'What is cooking?','How has your day been?']
+    user = await discord.fetch_user()
+    choice = random.choice(welcomes)
+    try:
+        guilds = await discord.fetch_guilds()
+    except:
+        return await redirect(url_for("login"))
+
+    clientguilds = await myipc.request('get_count')
+    print(clientguilds)
+    userguilds = []
+
+    for guild in guilds:
+        if guild in clientguilds:
+            userguilds.append(guild)
+
+
+    return await render_template("dashboard.html",user=user , choice=choice , guilds = userguilds)
+
+
+@app.route("/login")
+async def login():
+    return await discord.create_session()
+
+@app.route("/callback")
+async def callback():
+    try:
+        await discord.callback()
+    except Exception:
+        return redirect(url_for("login"))
+    
+    return redirect(url_for("dashboard"))
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
